939e0690066d037afe384cc58de7d1bc3ecd0069
Dawid Lazuk <dawid.lazuk@gmail.com>
2017-12-19 21:16:53 +0100

    test.json generation in App.xaml.cs ctor

--- ApproxSet/ApproxSetsApp/App.xaml.cs
+++ ApproxSet/ApproxSetsApp/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ApproximateSetsApp.Store;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,29 @@ namespace DecisionTreeApp
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            var store = new DataStore();
+            store.AttributeNames.Add("first");
+            store.AttributeNames.Add("second");
+            store.AttributeNames.Add("decision");
+
+            store.Elements.Add(new ReductDetection.ElementData()
+            {
+                Id = 0,
+                ConditionValues = new List<bool> { true, false },
+                DecisionValue = "result"
+            });
+
+            store.Elements.Add(new ReductDetection.ElementData()
+            {
+                Id = 1,
+                ConditionValues = new List<bool> { true, false },
+                DecisionValue = "otherResult"
+            });
+
+            store.Save("test.json");
+        }
+
     }
 }
